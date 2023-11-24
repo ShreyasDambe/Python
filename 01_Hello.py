5326a36f275df8e90c94d8328b266bb7f8b174f0
ShreyasDambe <101385887+ShreyasDambe@users.noreply.github.com>
2023-11-24 20:48:04 +0530

    Add files via upload

---
+++ 01_Hello.py
@@ -0,0 +1,9 @@
+#Author:Shreyas           # is use for single line Comment
+#Lisenced to:XYZ Company
+
+'''
+Author:Shreyas          triple single coat is use for multi line comment
+Lisenced to:XYZ Company
+'''
+import os #importing the OS Module
+print("Hello Shreyas")
